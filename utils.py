1042e839a48bd902ff418e6cf3cc088f93926351
matheus <91208015+robotheus@users.noreply.github.com>
2024-12-12 10:47:49 -0300

    Delete utils.py

--- utils.py
+++
@@ -1,6 +0,0 @@
-from ndlib.models.epidemics.IndependentCascadesModel import IndependentCascadesModel
-
-class ModifiedIC(IndependentCascadesModel):
-    def __init__(self, graph):
-        super().__init__(graph)
-        self.available_statuses = {"suscetível": 0, "ativo": 1}
